688f58bbb2e391cd2b922ed4117bc32894f8ecf1
CodeWithKarol <kmodelski93@gmail.com>
2025-12-29 11:22:30 +0100

    Revert "Replace placeholder app template with modern UI (#1)"
    This reverts commit 617bfbb3a8fb80d20942712814a0ffe6a0ad5c43.
    
    Revert "Update SVG xmlns and social icon path"
    
    This reverts commit 229a54ec9b1f6caf7d95b2eb5d995ff7702a4fdc.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  protected readonly title = signal('scale-sail-app');
+}
