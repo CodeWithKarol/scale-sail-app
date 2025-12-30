ebcd7dfc993e6cfb38a9ebdfce255bbd17e7fc32
CodeWithKarol <kmodelski93@gmail.com>
2025-12-30 11:07:23 +0100

    Refactor routing and update header navigation
    Replaced static main content components in app.html with a router outlet for dynamic routing. Defined routes for the hero and products components in app.routes.ts. Updated header navigation links to use Angular's routerLink for navigation to the products page, and added RouterLink to the header component imports.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./hero/hero').then((m) => m.Hero),
+  },
+  {
+    path: 'products',
+    loadComponent: () => import('./products/products').then((m) => m.Products),
+  },
+];
